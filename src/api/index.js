3aafcbbf33ffa9d72b6d19461bcc762970394f4e
Samuel Lin <samuellin@yahoo.com>
2024-05-31 16:53:57 -0700

    Make some stackoverflow changes

--- src/api/index.js
+++ src/api/index.js
@@ -11,20 +11,20 @@ API.interceptors.request.use((req) => {
 });
 
 //posts related API calls
-export const fetchPostsCount = () => API.get('hyrax/posts');
-export const fetchPostsBySearch = (searchQuery) => API.get(`hyrax/posts/search?searchQuery=${searchQuery || 'none'}`);
-export const fetchRelatedPosts = (searchQuery) => API.get(`hyrax/posts/relatedPosts?searchQuery=${searchQuery}`);
+export const fetchPostsCount = () => API.get('/posts');
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery || 'none'}`);
+export const fetchRelatedPosts = (searchQuery) => API.get(`/posts/relatedPosts?searchQuery=${searchQuery}`);
 
-export const fetchPostsByBatch = (batch) => API.get(`hyrax/posts/batch?batch=${batch || 'none'}`);
-export const fetchMyPosts = (creatorId) => API.get(`hyrax/posts/creatorId?creatorId=${creatorId || 'none'}`)
-export const fetchPost = (id) => API.get(`hyrax/posts/${id}`);
-export const createPost = (newPost) => API.post('hyrax/posts', newPost);
-export const getNotice = () => API.get('hyrax/noti/notifications');
-export const addNotice = (notice) => API.post(`hyrax/noti/notifications`, notice);
-export const getPrompt = (genTopic, subTopic, difficulty, number) => API.get(`hyrax/posts/${genTopic}/${subTopic}/${difficulty}/${number}`); // will need to add a problem parameter and fix link accordingly
+export const fetchPostsByBatch = (batch) => API.get(`/posts/batch?batch=${batch || 'none'}`);
+export const fetchMyPosts = (creatorId) => API.get(`/posts/creatorId?creatorId=${creatorId || 'none'}`)
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const getNotice = () => API.get('/noti/notifications');
+export const addNotice = (notice) => API.post(`/noti/notifications`, notice);
+export const getPrompt = (genTopic, subTopic, difficulty, number) => API.get(`/posts/${genTopic}/${subTopic}/${difficulty}/${number}`); // will need to add a problem parameter and fix link accordingly
 
 //authentification related calls
-export const signIn = (formData) => API.post('hyrax/user/signin', formData);
-export const googleSignIn = (formData) => API.post('hyrax/user/googleSignIn', formData);
-export const signUp = (formData) => API.post('hyrax/user/signup', formData);
-export const sendOTP = (finalData) => API.post('hyrax/user/sendotp', finalData);
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const googleSignIn = (formData) => API.post('/user/googleSignIn', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
+export const sendOTP = (finalData) => API.post('/user/sendotp', finalData);

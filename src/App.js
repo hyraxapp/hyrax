1a84c361eed6e5c7cf82bd1e3ceea237ba0908e1
Samuel Lin <samuellin@yahoo.com>
2024-05-31 21:30:05 -0700

    Try getting rid of basename router

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   return(
     <div>
       <Toaster/>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path='/' element={
           <div>

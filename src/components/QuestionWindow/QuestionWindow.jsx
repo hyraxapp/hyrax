797a458b7b4d23e3d4246eaa346498b7c09dada2
Samuel Lin <samuellin@yahoo.com>
2024-06-04 06:54:09 -0700

    test minor fix

--- src/components/QuestionWindow/QuestionWindow.jsx
+++ src/components/QuestionWindow/QuestionWindow.jsx
@@ -5,7 +5,7 @@ import './QuestionWindow.css';
 const seeProblem = async () => {
     try {
       const user = JSON.parse(localStorage.getItem("profile"));
-      const userTheta = user.theta;
+      const userTheta = user.result.theta;
       console.log("USER:");
       console.log(user);
       console.log("USER THETA");

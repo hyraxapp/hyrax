592556de378c4c97186e57c9998ba7cd0268a462
Samuel Lin <samuellin@yahoo.com>
2024-06-05 23:04:28 -0700

    test fix

--- src/components/AnswerBox/AnswerBox.jsx
+++ src/components/AnswerBox/AnswerBox.jsx
@@ -31,6 +31,8 @@ const AnswerBox = ({ id, theta, isMultipleChoice, isAnswerChoice, correctAnswer,
       setIsCorrect(cor);
       setIsSubmitted(true);
       const response = await accessParameters(id);
+      console.log("TEST");
+      console.log(response.a.$numberDecimal);
       const newVals = await getUpdatedParameters(theta, response.a.$numberDecimal, response.b.$numberDecimal, response.c.$numberDecimal, cor);
       await postUpdatedParameters(id, newVals.new_a, newVals.new_b);
       await updateTheta(id, newVals.theta);

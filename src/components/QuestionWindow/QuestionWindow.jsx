20e502af2f6d4fd82972e6b7791a3248568844f0
Samuel Lin <samuellin@yahoo.com>
2024-06-05 20:24:19 -0700

    fixes

--- src/components/QuestionWindow/QuestionWindow.jsx
+++ src/components/QuestionWindow/QuestionWindow.jsx
@@ -4,51 +4,49 @@ import {updateTheta} from '../../actions/auth';
 import AnswerBox from "../AnswerBox/AnswerBox";
 import './QuestionWindow.css';
 
-
-const seeProblem = async () => {
-    try {
-      const user = JSON.parse(localStorage.getItem("profile"));
-      const userTheta = parseFloat(user.result.theta.$numberDecimal);
-      console.log("USER THETA");
-      console.log(userTheta);
-      const questionId = await getBestQuestion(userTheta)
-      const response = await accessProblem(questionId.id);
-      const response2 = await getAnswer(questionId.id);
-      if (response) {
-        // const div = document.createElement('div');
-        // div.innerHTML = response.data;
-        const text = response.text;
-        const answers = response.answertext;
-        const questionBox = document.querySelector('.question-prompt');
-        questionBox.innerHTML = text;
-        const answerChoiceBox = document.querySelector('.answer-choices');
-        answerChoiceBox.innerHTML = answers;
-        const correctAnswer = response2.correct_answer;
-        const explanation = response2.text;
-        const isMultipleChoice = ['A', 'B', 'C', 'D'].includes(correctAnswer);
-        const isAnswerChoice = !isMultipleChoice;
-        setQuestionData({
-          id,
-          correctAnswer,
-          explanation,
-          isMultipleChoice,
-          isAnswerChoice,
-        });
-      }
-    } catch (error) {
-      console.log("Unable to see problem");
-      console.log(error);
-    }
-  };
-
 const QuestionWindow = () => {
   const user = JSON.parse(localStorage.getItem("profile"));
   const [html, setHtml] = useState('');
   const [questionData, setQuestionData] = useState(null);
 
   useEffect(() => {
+    const seeProblem = async () => {
+      try {
+        const user = JSON.parse(localStorage.getItem("profile"));
+        const userTheta = parseFloat(user.result.theta.$numberDecimal);
+        const questionId = await getBestQuestion(userTheta)
+        const response = await accessProblem(questionId.id);
+        const response2 = await getAnswer(questionId.id);
+        if (response) {
+          // const div = document.createElement('div');
+          // div.innerHTML = response.data;
+          const text = response.text;
+          const answers = response.answertext;
+          const questionBox = document.querySelector('.question-prompt');
+          questionBox.innerHTML = text;
+          const answerChoiceBox = document.querySelector('.answer-choices');
+          answerChoiceBox.innerHTML = answers;
+          const correctAnswer = response2.correct_answer;
+          const explanation = response2.text;
+          const isMultipleChoice = ['A', 'B', 'C', 'D'].includes(correctAnswer);
+          const isAnswerChoice = !isMultipleChoice;
+          const id = questionId;
+          setQuestionData({
+            id,
+            correctAnswer,
+            explanation,
+            isMultipleChoice,
+            isAnswerChoice,
+          });
+        }
+      } catch (error) {
+        console.log("Unable to see problem");
+        console.log(error);
+      }
+    };
+
     if (user) {
-      seeProblem(new XMLHttpRequest());
+      seeProblem();
     }
   }, [user]);
 

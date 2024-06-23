c36e8c3fdfef81ba0cf2453ba68b7e40f602d55c
Samuel Lin <samuellin@yahoo.com>
2024-06-23 10:52:11 -0400

    fix user validation

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const userSchema = Yup.object({
-    username : Yup.string().max(40).matches(/^[A-Za-z0-9]+$/, 'Only numbers and letters are allowed'),
+    username : Yup.string().max(40).matches(/^[A-Za-z0-9]+$/, {message:'Only numbers and letters are allowed', excludeEmptyString:true}),
     email : Yup.string().required(),
     password : Yup.string().required().min(6),
     money: 0.0,

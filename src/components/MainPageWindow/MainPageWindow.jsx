7028a244a72a60005dd5d3a8573b65bf9c7e7cb0
Samuel Lin <samuellin@yahoo.com>
2024-06-25 21:24:08 -0400

    second dumb change

--- src/components/MainPageWindow/MainPageWindow.jsx
+++ src/components/MainPageWindow/MainPageWindow.jsx
@@ -7,12 +7,12 @@ const MainPageWindow = () => {
         <>
         {user ? (
                 <div className='mainpage_container'>
-                    <div className="mainpage_header">SAT Math Prep</div>
+                    <div className="mainpage_header">Hyrax Dashboard | SAT Math Prep</div>
                     <div className="mainpage_box">Please Select One of the Fields on the Left to Begin</div>
                 </div>
             ) : (
                 <div className='mainpage_container'>
-                    <div className="mainpage_header">SAT Math Prep</div>
+                    <div className="mainpage_header">Hyrax Dashboard | SAT Math Prep</div>
                     <div className="mainpage_box">Welcome! Please sign in to start</div>
                 </div>
             )
